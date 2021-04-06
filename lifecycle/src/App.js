c7a3468e960c538e3fda66d36ca4c2314433b328
ALDO <aldo@aldos-mbp.attlocal.net>
2021-04-06 15:55:38 -0700

    mvp without search

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    user: "",
+    userName: "",
+    userPic: [],
+    userArea: "",
+    userCreation: "",
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/gamboaldo")
+      .then((res) => {
+        console.log(res.data);
+
+        this.setState({
+          user: res.data.login,
+          userPic: res.data.avatar_url,
+          userName: res.data.name,
+          userArea: res.data.location,
+          userCreation: res.data.created_at,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      user: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.user}`)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          user: res.data.login,
+          userPic: res.data.avatar_url,
+          userName: res.data.name,
+          userArea: res.data.location,
+          userCreation: res.data.created_at,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.user}
+            onChange={this.handleChange}
+            placeholder="enter user"
+          />
+          <button>Get Me The Dogs</button>
+        </form>
+        <h1>******github users*******</h1>
+        <h2>{[this.state.user]}</h2>
+        <h2>{[this.state.userName]}</h2>
+        <h2>{this.state.userArea}</h2>
+        <img src={this.state.userPic} alt="" />
+        <h1>{this.state.userCreation}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
